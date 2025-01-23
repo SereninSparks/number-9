bbb06d77d8a1bfa678bce4c94fea955b97c70c3a
SereninSparks <46284532+SereninSparks@users.noreply.github.com>
2025-01-23 21:47:41 +1000

    Add missing newlines

--- src/cpp/number-9.cpp
+++ src/cpp/number-9.cpp
@@ -5,9 +5,9 @@ int main() {
     srand(time(NULL));
 
     if (rand() % 9 + 1 == 9) {
-        std::cout << "Number 9...";
+        std::cout << "Number 9...\n";
     } else {
-        std::cout << "Try again.";
+        std::cout << "Try again.\n";
     }
 
     return 0;
